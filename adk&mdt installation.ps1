dc21db3704f789e0c67285704fea7dee9ccf8fbf
aeuPingLi <aeu3000327@aspire2student.ac.nz>
2023-09-08 12:23:29 +1200

    Part1 Install ADK and MDT!

---
+++ adk&mdt installation.ps1
@@ -0,0 +1,14 @@
+﻿#install adk
+cd "C:\asd\Windows Kits\10\ADK"
+
+start-Process -FilePath adksetup.exe -ArgumentList "/s" -Wait
+#install adkpe
+cd "C:\asd\Windows Kits\10\ADKWinPEAddons"
+
+start-Process -FilePath adkwinpesetup.exe -ArgumentList "/s" -Wait
+
+
+#install MDT, before that you need install ADK
+$mdtInstallerPath = "C:\asd\mdt.msi"
+Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$mdtInstallerPath`" /quiet /norestart" -Wait
+
